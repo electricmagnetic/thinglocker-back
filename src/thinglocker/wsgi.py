a28663dc2734e6682755ccfd22c6a6a9e27dab1b
George Moon <george@moon.org.nz>
2020-05-03 11:37:35 +1200

    Renaming

--- src/thinglocker/wsgi.py
+++ src/thinglocker/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for thingstore project.
+WSGI config for thinglocker project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thingstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thinglocker.settings')
 
 application = get_wsgi_application()
